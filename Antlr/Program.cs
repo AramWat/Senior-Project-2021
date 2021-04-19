2fb5293ca1339bde4d45d4fde0b5fff87dbd7489
TheDanchou <50221478+TheDanchou@users.noreply.github.com>
2021-04-18 23:06:17 -0400

    Update Program.cs

--- Antlr/Program.cs
+++ Antlr/Program.cs
@@ -8,22 +8,32 @@ using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
 using Antlr4.Runtime.Misc;
 
+
 namespace JavaCSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string input = "HelloWorldExample{ public static void main(String args[]){ System.out.println(\"Hello World !\");}}" ;
+            StreamReader sr = new StreamReader("W:/Spring2021/Senior proj/JavaCXGamesMode/JavaCSharp/JavaCSharp/Test.txt");
+            string input = sr.ReadToEnd();
             AntlrInputStream inputStream = new AntlrInputStream(input);
             ICharStream stream = inputStream;
             JavaLexer lexer = new JavaLexer(stream);
             ITokenStream tokens = new CommonTokenStream(lexer);
             JavaParser parser = new JavaParser(tokens);
-            parser.BuildParseTree = true;
             IParseTree tree = parser.compilationUnit();
-            Console.Write(tree);
+            //TokenStreamRewriter rewriter = new TokenStreamRewriter(tokens);
+            //Console.WriteLine(parser.ToString());
+            
+
+            //Console.WriteLine(tree.ToStringTree(parser));
+            JavaVisitor visitor = new JavaVisitor();
+            visitor.Visit(tree);
+
             Console.ReadLine();
+
         }
+
     }
 }
