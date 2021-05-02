d718d197348a5e5d63140ec401a2c1d32df1e88f
jhalimro <60119007+jhalimro@users.noreply.github.com>
2021-05-01 23:22:21 -0400

    Visitor by Jordan

---
+++ Antlr/JavaVisitor.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Antlr4.Runtime.Misc;
+
+namespace JavaCSharp
+{
+    class JavaVisitor : Java8ParserBaseVisitor<object>
+    {
+        String replace;
+        int startIndex;
+        int refIndex;
+        String newBody;
+        public override object VisitCompilationUnit([NotNull] Java8Parser.CompilationUnitContext context)
+        {
+            
+            return VisitChildren(context);
+        }
+
+        public override object VisitTypeDeclaration([NotNull] Java8Parser.TypeDeclarationContext context)
+        {
+ 
+            return base.VisitTypeDeclaration(context);
+        }
+
+        public override object VisitClassOrInterfaceType([NotNull] Java8Parser.ClassOrInterfaceTypeContext context)
+        {
+            //Nothing inside body
+            return base.VisitClassOrInterfaceType(context);
+        }
+
+        public override object VisitClassDeclaration([NotNull] Java8Parser.ClassDeclarationContext context)
+        {
+            String body = context.GetText();
+            Console.WriteLine("Class Declaration Here" + "\n" + body);
+            return base.VisitClassDeclaration(context);
+        }
+
+        public override object VisitClassBody([NotNull] Java8Parser.ClassBodyContext context)
+        {
+            String body = context.GetText();
+            if (body.Contains("publicstaticvoidmain(String[]args){"))
+            {
+                startIndex = body.IndexOf("args){") + 6;
+                replace = "public static void main(String[]args)";
+            }
+            Console.WriteLine("\n" + replace + "\n" + "{");
+            newBody = body.Substring(startIndex);
+            return base.VisitClassBody(context);
+        }
+
+        public override object VisitMethodBody([NotNull] Java8Parser.MethodBodyContext context)
+        {
+            newBody = newBody.Replace("int", " int ").Replace(";",";\n");
+            //for(int i = 0; i < newBody.Length;i++)
+            //{
+            //    if (newBody.Contains("int"))
+            //    {
+            //        refIndex = newBody.IndexOf("int");
+            //    }
+            //}
+            Console.WriteLine(newBody);
+            return base.VisitMethodBody(context);
+        }
+
+        public override object VisitBlock([NotNull] Java8Parser.BlockContext context)
+        {
+            Console.WriteLine("Block here");
+            return base.VisitBlock(context);
+        }
+
+        public override object VisitBlockStatement([NotNull] Java8Parser.BlockStatementContext context)
+        {
+            Console.WriteLine("block statement");
+            return base.VisitBlockStatement(context);
+        }
+
+        public override object VisitStatement([NotNull] Java8Parser.StatementContext context)
+        {
+            Console.WriteLine("statement");
+            return base.VisitStatement(context);
+        }
+
+        public override object VisitExpression([NotNull] Java8Parser.ExpressionContext context)
+        {
+            Console.WriteLine("expression here");
+            return base.VisitExpression(context);
+        }
+
+        
+    }
+
+    
+}
